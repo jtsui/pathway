38c66adaa70658ac0961854eb20ab7a56b9fd8d2
Jeff Tsui <tsui.jeff@gmail.com>
2012-11-08 13:02:16 -0800

    framework up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def hiring
+  end
+  
+  def act
+  end
+  
+  def people
+  end
+  
+  def talks
+  end
+
+  def papers
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Pathway::Application.routes.draw do
   root :to => "home#index"
+  match '/hiring' =>'home#hiring'
+  match '/act' =>'home#act'
+  match '/talks' =>'home#talks'
+  match '/papers' =>'home#papers'
+  match '/people' =>'home#people'
 end
