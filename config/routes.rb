d3d92731ea557ef8b29fe05e2205418e05dee040
Jeff Tsui <tsui.jeff@gmail.com>
2013-06-27 00:12:00 -0700

    add links to synthesizer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Pathway::Application.routes.draw do
-  root :to => "home#index"
+  # root :to => "home#index"
+  root :to => redirect('http://pathway.berkeley.edu/synthesizer')
+  match '/synthesizer' => redirect('http://pathway.berkeley.edu/synthesizer')
   match '/hiring' =>'home#hiring'
   match '/act' =>'home#act'
   match '/talks' =>'home#talks'
